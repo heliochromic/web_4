2f67961d55a1997696e75bd35a62e2d4f0ea411e
bohdan <howchromium@gmail.com>
2023-06-08 14:40:37 +0300

    pizza time

---
+++ page/www/scripts/init.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cart = document.getElementById("cart");
+
+  fetch("./scripts/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Loop through each pizza object in the JSON data
+      data.forEach((pizza) => {
+        // Create a new pizza card element
+        const card = document.createElement("div");
+        card.className = "pizza-panel";
+        let content = "";
+
+        for (const key in pizza.content) {
+          const elements = pizza.content[key];
+          content += elements.join(", ") + ", ";
+        }
+
+        const contentString =
+          content.charAt(0).toUpperCase() + content.slice(1);
+
+        card.innerHTML = `
+                    <div class="pizza-card">
+                        <img src="${pizza.icon}" />
+        
+                        <div class="caption">
+                        <h3>${pizza.title}</h3>
+
+                        <p class="pizza-type">${pizza.type}</p>
+                        <p>${contentString}</p>
+                        </div>
+                        <div class="order-buttons">
+                        <div class="buy_button">
+                          <div class="svg-container">
+                            <img src="assets/images/size-icon.svg" alt="size-icon" />
+                            ${pizza.small_size.size}
+                          </div>
+                          <div class="svg-container">
+                            <img src="assets/images/weight.svg" alt="size-icon" />
+                            ${pizza.small_size.weight}
+                          </div>
+                          <div class="price-container">
+                            <div class="price">${pizza.small_size.price}</div>
+                            <div class="grn">грн</div>
+                          </div>
+                          <button class="add-to-order">Купити</button>
+                        </div>
+                        <div class="buy_button">
+                          <div class="svg-container">
+                            <img src="assets/images/size-icon.svg" alt="size-icon" />
+                            ${pizza.big_size.size}
+                          </div>
+                          <div class="svg-container">
+                            <img src="assets/images/weight.svg" alt="size-icon" />
+                            ${pizza.big_size.weight}
+                          </div>
+                          <div class="price-container">
+                            <div class="price">${pizza.big_size.price}</div>
+                            <div class="grn">грн</div>
+                          </div>
+                          <button class="add-to-order">Купити</button>
+                        </div>
+                      </div>
+                    </p>
+                    </div>
+                `;
+
+        // Append the pizza card to the cart
+        cart.appendChild(card);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+console.log("hello world");
